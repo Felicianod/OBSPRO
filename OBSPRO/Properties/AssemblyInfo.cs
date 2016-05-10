a95f5b0c9847bac4ac1094bb0ff9fe89889c915e
Rasul Abduguev <rasul.abduguev@dsc-logistics.com>
2016-05-10 14:48:53 -0500

    5/10 2:48

--- OBSPRO/Properties/AssemblyInfo.cs
+++ OBSPRO/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WebApplication1")]
+[assembly: AssemblyTitle("OBSPRO")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("WebApplication1")]
+[assembly: AssemblyProduct("OBSPRO")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
