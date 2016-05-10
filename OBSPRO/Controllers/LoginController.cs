4b9268d05c2e837991fe867cf4bc082175407190
FELICIANO DELGADO <feliciano.delgado@dsc-logistics.com>
2016-05-10 13:29:49 -0500

    .

---
+++ OBSPRO/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace OBSPRO.Controllers
+{
+    [AllowAnonymous]
+    public class LoginController : Controller
+    {
+
+        [HttpGet]
+        public ActionResult OBSLogin() { return View(); } 
+
+
+        public ActionResult OBSLogout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login", "Login");
+        }
+    }
+}
