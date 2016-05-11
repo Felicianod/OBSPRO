888d6ee13ab22cc1489efe016238d4a25609f4c0
Rasul Abduguev <rasul.abduguev@dsc-logistics.com>
2016-05-11 15:39:34 -0500

    5/11 3:39

--- OBSPRO/Models/Observation.cs
+++ OBSPRO/Models/Observation.cs
@@ -15,5 +15,35 @@ namespace OBSPRO.Models
    
         [Display(Name = "User Name")]
         public string user_name { get; set; }
+
+        [Display(Name = "Form Instance Id")]
+        public string form_inst_id { set; get; } 
+
+        [Display(Name = "Observer Id")]
+        public string observer_id { set; get; }
+
+        [Display(Name = "Observed Id")]
+        public string observed_id { set; get; }
+
+        [Display(Name = "Status")]
+        public string status { set; get; }
+
+        [Display(Name = "First Name")]
+        public string observer_first_name { set; get; }
+
+        [Display(Name = "Last Name")]
+        public string observed_last_name { set; get; }
+
+
+        [Display(Name = "ADP ID")]
+        public string observed_adp_id { set; get; }
+
+
+        [Display(Name = "Form Title")]
+        public string form_title { set; get; }
+
+        [Display(Name = "Observation Start Time")]
+        public string obs_start_time { set; get; }
+
     }
 }
